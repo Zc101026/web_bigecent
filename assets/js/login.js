02e703ca4b3eeb1f834433d15bd0e41efe216bda
zhangchao <784769240@qq.com>
2020-06-27 20:23:18 +0800

    辛苦了，完成！

---
+++ assets/js/login.js
@@ -0,0 +1,73 @@
+$(function () {
+    //  点击 “去注册账号” 的连接
+    $('#link_reg').on('click', function () {
+        $('.login_box').hide()
+        $('.reg_box').show()
+    })
+    //  点击 “去登录账号” 的链接
+    $('#link_login').on('click', function () {
+        $('.reg_box').hide()
+        $('.login_box').show()
+    })
+})
+// 自定义校验规则
+const form = layui.form
+const layer = layui.layer
+// 注册密码
+form.verify({
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function(value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败,则return一个提示消息即可
+      var pwd = $('.reg_box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+})
+
+// 监听 ： “去注册” 点击的事件
+$('#form_reg').on('submit', function (e) {
+    // 阻止默认的提交行为
+    e.preventDefault()
+    // 发起 Ajax的POST的请求
+    const data = {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function (res) {
+        if (res.status !== 0) {
+            return layer.msg(res.message)
+        }
+        layer.msg('注册成功，请登录！')
+        // 注册后跳转登录页面
+        $('#link_login').click()
+    })
+})
+
+// 监听登录表单的提交事件
+$('#form_login').submit(function (e) {
+    // 阻止默认提交行为
+    e.preventDefault()
+    $.ajax({
+        url: '/api/login',
+        method:'POST',
+        // 快速获取表单中的数据
+        data: $(this).serialize(),
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('登录成功！')
+            // 将登录成功得到的 token 字符串， 保存到 localStorage 中
+            // console.log(res.token)
+            localStorage.setItem('token', res.token)
+            // 跳转到后台主页
+            location.href = '/index.html'
+        }
+    })
+})
